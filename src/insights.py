7f6ebaa12be80feda4d279d0e0deb3e59ab227e2
Diogo San'Anna <diogo.santanna@outlook.com.br>
2022-05-31 16:59:59 -0300

    feat: function "get_unique_job_types" created

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,15 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
+    info_jobs = read(path)
+    job_types = list()
 
-    Must call `read`
+    for job in info_jobs:
+        if job["job_type"] not in job_types:
+            job_types.append(job["job_type"])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    return job_types
 
 
 def filter_by_job_type(jobs, job_type):
