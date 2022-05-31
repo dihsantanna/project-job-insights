51292d1b8458c007e0ee81aeaf37e0b3897bbb7f
Diogo San'Anna <diogo.santanna@outlook.com.br>
2022-05-31 15:17:36 -0300

    feat: function read created

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,27 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    try:
+        print(f"Abrindo arquivo no caminho '{path}'")
+        with open(path, encoding="utf-8") as file:
+            jobs_reader = csv.reader(file, delimiter=",", quotechar='"')
+            header, *data = jobs_reader
+            info_jobs = list()
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
+            for job in data:
+                info = dict()
+                for index in range(len(header)):
+                    info[header[index]] = job[index]
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+                info_jobs.append(info)
+
+            print("Manipulação de arquivo feita com sucesso")
+            return info_jobs
+
+    except OSError:
+        print("Arquivo inexistente")
+    finally:
+        print("Processamento Encerrado")
