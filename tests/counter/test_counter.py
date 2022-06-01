55501de0c7e9166c5b0d2549c461702711d482f7
Diogo San'Anna <diogo.santanna@outlook.com.br>
2022-06-01 07:22:55 -0300

    tests: test_counter created

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,16 @@
-# from src.counter import count_ocurrences
+import pytest
+from src.counter import count_ocurrences
+
+path = "src/jobs.csv"
+word1 = "Python"
+word2 = "Javascript"
 
 
 def test_counter():
-    pass
+    assert count_ocurrences(path, word1) == 1639
+
+    assert count_ocurrences(path, word2) == 122
+
+    with pytest.raises(AssertionError):
+        assert count_ocurrences(path, word1) == 122
+        assert count_ocurrences(path, word2) == 1639
