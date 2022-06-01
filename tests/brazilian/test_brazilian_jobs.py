eb8854ca0b48d0b26f1738f440ef46008cb777fd
Diogo San'Anna <diogo.santanna@outlook.com.br>
2022-06-01 07:44:53 -0300

    tests: test_brazilian_jobs created

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,14 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+
+path = "tests/mocks/brazilians_jobs.csv"
+expected = ["title", "salary", "type"]
 
 
 def test_brazilian_jobs():
-    pass
+    for job in read_brazilian_file(path):
+        keys = job.keys()
+
+        assert len(keys) == len(expected)
+
+        for key in keys:
+            assert key in expected
